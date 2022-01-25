d9057d57b0b54bca1991847aa77d7bdebe1bfaae
adithiarisma <heyyohapvi@gmail.com>
2022-01-25 22:48:17 +0700

    commit ch 5

--- (5) Class/class.js
+++ (5) Class/class.js
@@ -44,4 +44,98 @@ var array1 = {
   },
 };
 
+//3
+a = 20;
+var array1 = {
+  Array0: [29, 12, 30, "ini", "saya", 85],
+  Array2: ["cyber", "security", 20, 15, 21],
+  soal1: function() {
+    var arrayLength1 = Array0.length;
+    var arrayLength2 = Array2.length;
+    if (arrayLength2 + a == 28) {
+      for (var i = 0; i < arrayLength1; i++) {
+        if (arrayLength1 % i == 0) {
+          sum = arrayLength1 + arrayLength2;
+          console.log(sum);
+        } else {
+          console.log(arrayLength1.arrayLength2);
+        }
+      }
+    } else {
+      console.log(false);
+    }
+    return this.soal1;
+  },
+};
 
+//4
+a = 20;
+var array1 = {
+  Array0: [29, 12, 30, "ini", "saya", 85],
+  Array2: ["cyber", "security", 20, 15, 21],
+  soal1: function() {
+    var arrayLength1 = Array0.length;
+    var arrayLength2 = Array2.length;
+    if (arrayLength2 + 1 != 35) {
+      for (var i = 0; i < arrayLength1; i++) {
+        if (arrayLength1 % i == 0) {
+          sum = arrayLength1 + arrayLength2;
+          console.log(sum);
+        } else {
+          console.log(arrayLength1.arrayLength2);
+        }
+      }
+    } else {
+      console.log(false);
+    }
+    return this.soal1;
+  },
+};
+
+//5
+a = 20;
+var array1 = {
+  Array0: [29, 12, 30, "ini", "saya", 85],
+  Array2: ["cyber", "security", 20, 15, 21],
+  soal1: function() {
+    var arrayLength1 = Array0.length;
+    var arrayLength2 = Array2.length;
+    if (arrayLength2 != 35) {
+      for (var i = 0; i < arrayLength1; i++) {
+        if (arrayLength1 % i == 0) {
+          sum = arrayLength1 + arrayLength2;
+          console.log(sum);
+        } else {
+          console.log(arrayLength1.arrayLength2);
+        }
+      }
+    } else {
+      console.log(false);
+    }
+    return this.soal1;
+  },
+};
+
+//6
+u = 20;
+var array1 = {
+  Array0: [29, 12, 30, "ini", "saya", 85],
+  Array2: ["cyber", "security", 20, 15, 21],
+  soal1: function() {
+    var arrayLength1 = Array0.length;
+    var arrayLength2 = Array2.length;
+    if (arrayLength2 + arrayLength1 + u != 25) {
+      for (var i = 0; i < arrayLength1; i++) {
+        if (arrayLength1 % i == 0) {
+          sum = arrayLength1 + arrayLength2;
+          console.log(sum);
+        } else {
+          console.log(arrayLength1.arrayLength2);
+        }
+      }
+    } else {
+      console.log(false);
+    }
+    return this.soal1;
+  },
+};
